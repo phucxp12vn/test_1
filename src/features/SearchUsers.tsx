d45828215a2ce2cc722b5d755bd814db0bb90e4d
phucxp12vn <vphuc8230tth@gmail.com>
2024-04-22 11:25:53 +0700

    refractory component add implement spinner progress

--- src/features/SearchUsers.tsx
+++ src/features/SearchUsers.tsx
@@ -11,17 +11,57 @@ import {
   Avatar,
   TextField,
   Box,
+  CircularProgress,
 } from "@mui/material";
 
-import { fetchUsers, clearUsers } from './SearchUsersSlice';
+import { fetchUsers, clearUsers } from "./SearchUsersSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hook";
+
+function DisplayUserTable() {
+  const users = useAppSelector((state) => state.users.users);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Avatar</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell align="right">score</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow
+              key={user.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <Avatar
+                  alt={`avatar of user ${user.login}`}
+                  src={user.avatar_url}
+                />
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {user.login}
+              </TableCell>
+              <TableCell>{user.type}</TableCell>
+              <TableCell align="right">{100}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 function SearchUsers() {
   const [inputQuery, setInputQuery] = useState("");
-
-  const dispatch = useAppDispatch()
-  const users = useAppSelector((state) => state.users.users)
-
+  const fetchUsersStatus = useAppSelector(
+    (state) => state.users.fetchUsersStatus
+  );
+  const dispatch = useAppDispatch();
 
   const handleSearchUser = (data: string) => {
     dispatch(fetchUsers(data));
@@ -45,47 +85,26 @@ function SearchUsers() {
 
   return (
     <>
-      <Box mb={2}>Search Github Users</Box>
-      <TextField
-        size="small"
-        id="search-field"
-        label="Search users"
-        variant="outlined"
-        value={inputQuery}
-        onChange={handleInputQueryUser}
-      />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Avatar</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Type</TableCell>
-              <TableCell align="right">score</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow
-                key={user.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Avatar
-                    alt={`avatar of user ${user.login}`}
-                    src={user.avatar_url}
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {user.login}
-                </TableCell>
-                <TableCell>{user.type}</TableCell>
-                <TableCell align="right">{100}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Box mb={2}>
+        <Box mb={2}>Search Github Users</Box>
+        <TextField
+          size="small"
+          id="search-field"
+          label="Search users"
+          variant="outlined"
+          value={inputQuery}
+          onChange={handleInputQueryUser}
+        />
+      </Box>
+      {fetchUsersStatus === "PENDING" && <CircularProgress />}
+      {fetchUsersStatus === "SUCCESS" && (
+        <Box>
+          <DisplayUserTable />
+        </Box>
+      )}
+      {fetchUsersStatus === "ERROR" && (
+        <p>There was a problem searching users</p>
+      )}
     </>
   );
 }
